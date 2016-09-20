5e440d33afa7559407a00852c47224315c043f85
Raymond Usbal <raymond@philippinedev.com>
2016-09-20 09:52:22 +0800

    names table

---
+++ database/migrations/2016_09_20_011916_create_names_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('names', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('meaning_function');
+            $table->text('identical_titles');
+            $table->text('significance');
+            $table->text('responsibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('names');
+    }
+}
