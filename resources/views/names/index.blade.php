4043d973a894db9b041fa2448c26ca913b9bba39
Raymond Usbal <raymond@philippinedev.com>
2016-09-22 00:00:58 +0800

    panel heading with button

--- resources/views/names/index.blade.php
+++ resources/views/names/index.blade.php
@@ -5,7 +5,11 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Names List<a href="{{ url('/names/new') }}" class="btn btn-primary">New</a></div>
+                <div class="panel-heading">
+                    <h3 class="panel-title pull-left"> Names List </h3>
+                    <a href="{{ url('/names/new') }}" class="btn btn-primary pull-right">New</a>
+                    <div class="clearfix"></div>
+                </div>
 
             @if (session('status'))
                 <div class="alert alert-success">
