fc51250681e9fc8bc121426da7d30c97c9b7e83f
Raymond Usbal <raymond@philippinedev.com>
2016-10-09 22:21:58 +0800

    unique color validation

--- app/Http/Requests/UserProfileRequest.php
+++ app/Http/Requests/UserProfileRequest.php
@@ -26,7 +26,7 @@ class UserProfileRequest extends FormRequest
         return [
             'name'     => 'required|min:3',
             'initials' => 'min:2|max:4',
-            'color'    => 'required|unique:user|color'
+            'color'    => 'required|unique:users|color'
         ];
     }
 }
